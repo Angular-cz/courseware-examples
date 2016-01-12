88167a380b81664e4deabc3ebbfafa08cbec28f7
Víťa Plšek <vita@angular.cz>
2016-01-12 01:36:22 +0100

    determine exercise name in courseware

--- 02-generator-with-tests/app.spec.js
+++ 02-generator-with-tests/app.spec.js
@@ -2,4 +2,8 @@ describe('Tests test (TODO 1.1)', function() {
   it('true', function() {
     expect(true).toBeTruthy();
   });
+
+  it('xx', function() {
+    expect(true).toBeTruthy();
+  });
 });
--- base-karma.conf.js
+++ base-karma.conf.js
@@ -37,18 +37,16 @@ module.exports = function(config) {
   config.plugins.push('karma-spec-json-reporter');
   config.reporters.push('specjson');
 
-  // configure proper filename
-  var configDir = path.dirname(config.configFile);
-  var exerciseName = path.basename(configDir);
-
   var courseware = require("angular-cz-courseware");
 
   var testResultsDir = courseware.getTestResultsDir();
+
   if (!fs.existsSync(testResultsDir)){
     fs.mkdirSync(testResultsDir);
     console.log('Directory for test results has been created: ' + testResultsDir);
   }
 
+  var exerciseName = courseware.determineExerciseName(config);
   var outPath = courseware.getTestResultsFilename(exerciseName);
 
   config.specjsonReporter = {
